ac7294203c657030684f9789fbe7e9a402156ab2
Nenad Milutinovic <nenad.milutinovic@hhinc.eu>
2020-01-21 16:29:14 +0100

    top navigation removed, added functionality when choosing category

---
+++ js/main.js
@@ -0,0 +1,27 @@
+const listItems = document.querySelectorAll('.content-side-menu__item')
+
+const showItem = e => {
+  e.stopPropagation()
+
+  const selectedElement = document.querySelector('.display')
+    
+  if (selectedElement) {
+    const selectedElementId = selectedElement.id.split('-')[1]
+    const newSelectedElementId = e.currentTarget.dataset.id
+    
+    if (newSelectedElementId !== selectedElementId) {
+      selectedElement.classList.toggle('display')
+      selectedElement.classList.toggle('display-no')
+      document.querySelector('#feature-' + newSelectedElementId).classList.toggle('display')
+      document.querySelector('#feature-' + newSelectedElementId).classList.toggle('display-no')
+      document.querySelector(`[data-id="${newSelectedElementId}"]`).classList.toggle('active')
+      document.querySelector(`[data-id="${selectedElementId}"]`).classList.toggle('active')
+    }
+
+    return
+  }
+}
+
+listItems.forEach(item => {
+  item.addEventListener('click', showItem)
+})
